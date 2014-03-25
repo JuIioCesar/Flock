01e7d037a46ccb7be7c62f88ee265e9af54fd745
JuIioCesar <julio.guzman@me.com>
2014-03-25 17:49:52 -0600

    Initial Commit

---
+++ Flock/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Flock
+//
+//  Created by Julio César Guzman on 3/25/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
