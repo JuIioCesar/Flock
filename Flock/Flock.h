b0ee2a13f2e11658be6eacb46bf33824b7f37ef0
Julio Guzman <julio.guzman@me.com>
2017-08-04 23:42:24 -0500

    Create Flock framework

---
+++ Flock/Flock.h
@@ -0,0 +1,19 @@
+//
+//  Flock.h
+//  Flock
+//
+//  Created by Julio Cesar Guzman Villanueva on 8/4/17.
+//  Copyright © 2017 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Flock.
+FOUNDATION_EXPORT double FlockVersionNumber;
+
+//! Project version string for Flock.
+FOUNDATION_EXPORT const unsigned char FlockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Flock/PublicHeader.h>
+
+
